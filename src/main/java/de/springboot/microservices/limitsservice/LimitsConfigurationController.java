5f7abadaf2dcfd69d76389d11a2cece1d72260d7
evangelopoulos <jamesevag@hotmail.com>
2018-09-16 22:44:43 +0200

    initial

---
+++ src/main/java/de/springboot/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package de.springboot.microservices.limitsservice;
+
+import de.springboot.microservices.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitConfig(){
+
+        return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+ }
---
+++ src/main/java/de/springboot/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,33 @@
+package de.springboot.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+
+    private int maximum;
+    private int minimum;
+
+    protected LimitConfiguration() {
+
+    }
+
+    public LimitConfiguration(int max, int min) {
+        this.maximum=max;
+        this.minimum=min;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+}
